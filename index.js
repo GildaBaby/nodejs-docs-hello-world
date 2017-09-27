dc32ead1ab5ce8e7b33897dfa474f3bc4aa8afc5
gildelz <gildababy@gmail.com>
2017-09-27 15:53:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World from Jenkins!");
-
+    response.end("Hello World from Jenkins !");
+    response.end("Hello World from Docker!");
 });
 
 var port = process.env.PORT || 1337;
